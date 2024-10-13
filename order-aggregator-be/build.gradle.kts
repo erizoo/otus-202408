88c7ae1dcd127e6a13742e616410368352ecb026
Александр Бойко <boiko.erizoo@gmail.com>
2024-10-13 18:17:29 +0300

    M2L5

---
+++ order-aggregator-be/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+}
+
+group = "by.boiko.otus.order.aggregator"
+version = "0.0.1"
+
